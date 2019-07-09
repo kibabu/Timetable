1c0da7e08502a05d3a0dfbb20e64fa34bee3af74
Bantura Kibabu <banturagibson@gmail.com>
2019-07-09 16:56:18 +0300

    Initial Commits

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  $config = array (
+  'db_host' => 'localhost',
+  'db_name' => 'school',
+  'db_user' => 'bantura',
+  'db_pswd' => 'omino2019',
+  'ldap_host' => '',
+  'ldap_dn' => '',
+);
+?>
